ba2179c3ed934439793825381e7ccf12e5e45980
GwnGuillaume <51330225+GwnGuillaume@users.noreply.github.com>
2021-08-20 14:32:17 +0200

    Update build_geos.cmd

--- build_geos.cmd
+++ build_geos.cmd
@@ -15,11 +15,20 @@ if exist %GEOS_INSTALL% (
   pip install ninja
   cmake --version
 
+REM  mkdir build
+REM  cd build
+REM  cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=%GEOS_INSTALL% .. || exit /B 1
+REM  cmake --build . --config Release || exit /B 2
+REM  ctest . --config Release || exit /B 3
+REM  cmake --install . --config Release || exit /B 4
+REM  cd ..
   mkdir build
   cd build
   cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=%GEOS_INSTALL% .. || exit /B 1
   cmake --build . --config Release || exit /B 2
-  ctest . --config Release || exit /B 3
+  REM disable ctest because of issue with GEOS 3.9.0
+  REM see https://trac.osgeo.org/geos/ticket/1081
+  REM ctest . --config Release || exit /B 3
   cmake --install . --config Release || exit /B 4
   cd ..
 )
